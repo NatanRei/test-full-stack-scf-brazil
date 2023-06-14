400b1442472bd5e0876b93cf074d428851103726
NatanRei <nreischmura@gmail.com>
2023-06-14 09:35:02 -0300

    feat: adição de middlewares nas rotas de edição e deleção

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,13 +5,15 @@ import { update } from "./update";
 import { remove } from "./remove";
 import { create } from "./create";
 import { countReadedTimes } from "./count-readed-times";
+import { verifyUserRole } from "@/http/middlewares/verify-user-role";
+import { verifyJWT } from "@/http/middlewares/verify-jwt";
 
 export async function usersRoutes(app: FastifyInstance) {
     app.get('/users', get)
     app.get('/users/:id', find)
     app.get('/users/:id/readed-times', countReadedTimes)
     app.post('/users', create)
-    app.patch('/users/:id', update)
-    app.delete('/users/:id', remove)
+    app.patch('/users/:id', { onRequest: [verifyJWT, verifyUserRole('ADMIN')]}, update)
+    app.delete('/users/:id', { onRequest: [verifyJWT, verifyUserRole('ADMIN')]}, remove)
 
 }
--- src/http/middlewares/verify-user-role.ts
+++ src/http/middlewares/verify-user-role.ts
@@ -1,10 +1,10 @@
-// import { FastifyReply, FastifyRequest } from "fastify";
+import { FastifyReply, FastifyRequest } from "fastify";
 
-// export function verifyUserRole(roleToVerify: 'ADMIN' | 'COMMON') {
-//     return async ( request: FastifyRequest, reply: FastifyReply ) => {
-//         const { role } = request.user
-//         if (role !== roleToVerify) {
-//             return reply.status(401).send({ message: 'Unauthorized.' })
-//         }
-//     }
-// }
+export function verifyUserRole(roleToVerify: 'ADMIN' | 'COMMON') {
+    return async ( request: FastifyRequest, reply: FastifyReply ) => {
+        const { role } = request.user
+        if (role !== roleToVerify) {
+            return reply.status(401).send({ message: 'Unauthorized.' })
+        }
+    }
+}
